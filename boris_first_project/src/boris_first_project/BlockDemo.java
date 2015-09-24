999f7f61cdbf7a362317e9f3a456a5a7cb3ff06d
Boris <brizov86@gmail.com>
2015-09-24 10:32:53 +0100

    Adding the important stuff from work. U PICKU MATERINU!!

---
+++ boris_first_project/src/boris_first_project/BlockDemo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package boris_first_project;
+
+/**
+ * @author borisr
+ *
+ */
+public class BlockDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean condition = false;
+		if(condition){
+			System.out.println("ebanie is true");
+		}
+		else {
+			System.out.println("Ebanie is false");
+		}
+	}
+
+}
---
+++ boris_first_project/src/boris_first_project/helou_world_maafaka.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package boris_first_project;
+
+/**
+ * @author borisr
+ *
+ */
+public class helou_world_maafaka {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ebanieeee");
+	}
+
+}

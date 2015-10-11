91e2fe9b18d507aa024f058873da58c757148f49
Boris <brizov86@gmail.com>
2015-10-11 01:30:14 +0100

    ujasni lambda gluposti

---
+++ Hello World App/src/anonimusClassesOracle/Person.java
@@ -0,0 +1,24 @@
+package anonimusClassesOracle;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+    }
+
+    public void printPerson() {
+        // ...
+    }
+
+}
